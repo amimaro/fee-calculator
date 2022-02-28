79fbbd46cbfc1919dbba19e62459929d4cba5550
amimaro <amir.zahlan@gmail.com>
2022-02-28 17:10:35 -0300

    Update api key

--- app/services/gas-tracker.service.tsx
+++ app/services/gas-tracker.service.tsx
@@ -1,9 +1,9 @@
 export const getGasTrack = async () => {
   const ethereumGasTrack = await fetch(
-    "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=PCCB2YEAI5529APMX36Z6XW95K2D9WS36F"
+    `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETHERSCAN_API_KEY}`
   );
   const polygonGasTrack = await fetch(
-    "https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=GX3BXKG4YWZBNHESGVVA93D46ME2AWZ5TR"
+    `https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=${process.env.POLYGONSCAN_API_KEY}`
   );
   return {
     ethereum: (await ethereumGasTrack.json()).result,
