c01ff1703539a38fe82a0a0e55e4e07e06f5867e
amimaro <amir.zahlan@gmail.com>
2022-02-28 05:55:06 -0300

    Global reset and fonts setup

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,30 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import CssBaseline from '@mui/material/CssBaseline';
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
+};
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+    },
+  ];
 };
 
 export default function App() {
@@ -22,6 +37,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <CssBaseline />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
